ad7e5d5b4c79735311134e2111ce2b12251a433b
vj <sektaaccel@gmail.com>
2021-01-19 20:49:59 +0300

    bind exchange for simple message

--- src/bomber/rabbit_connector.clj
+++ src/bomber/rabbit_connector.clj
@@ -3,15 +3,17 @@
             [langohr.channel :as lch]
             [langohr.queue :as lq]
             [langohr.consumers :as lc]
-            [langohr.basic :as lb]
+            [langohr.exchange :as lex]
             [bomber.consumer.bomb-handler :as handler])
   (:gen-class))
 
 (defn init-connect
   [settings]
-  (let [conn  (rmq/connect settings)
-        ch    (lch/open conn)
-        qname "simple-bomb"]
-    (lq/declare ch qname {:exclusive false :auto-delete false})
+  (let [conn   (rmq/connect settings)
+        ch     (lch/open conn)
+        qname  "simple-bomb-queue"
+        exname "simple-bomb"
+        queue  (.getQueue (lq/declare ch qname {:exclusive false :auto-delete false}))]
+    (lex/fanout ch exname {:auto-delete false})
+    (lq/bind ch queue exname)
     (lc/subscribe ch qname handler/on-simple-message {:auto-ack true})))
-
